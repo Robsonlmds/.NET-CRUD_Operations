05e4f552de5d981445e6441aa9ba8faa6f9536d8
RobsonLMDS <98341530+Robsonlmds@users.noreply.github.com>
2024-10-01 18:27:29 -0300

    Adicionar arquivos de projeto.

---
+++ CRUD_Operations/Models/PaymentDetailContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CRUD_Operations.Models
+{
+    public class PaymentDetailContext : DbContext
+    {
+        public PaymentDetailContext(DbContextOptions options) : base(options) 
+        {
+               
+        }
+
+        public DbSet<PaymentDetail> paymentDetails { get; set; }
+    }
+}
